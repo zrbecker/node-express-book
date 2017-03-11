ddb8845a6801c161a331172ca2fdb2950c83b5e6
Zachary Becker <Zachary Becker>
2017-03-10 22:10:21 -0800

    Added handlebars rendering to express.

--- chapter03/meadowlark/site/meadowlark.js
+++ chapter03/meadowlark/site/meadowlark.js
@@ -2,31 +2,33 @@ var express = require('express');
 
 var app = express();
 
+// set up handlebars view engine
+var handlebars = require('express3-handlebars')
+    .create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
-  res.type('text/plain');
-  res.send('Meadowlark Travel');
+  res.render('home');
 });
 
 app.get('/about', function(req, res) {
-  res.type('text/plain');
-  res.send('About Meadowlark Travel');
+  res.render('about');
 });
 
 // custom 404 page
 app.use(function(req, res, next) {
-  res.type('text/plain');
   res.status(404);
-  res.send('404 - Not Found');
+  res.render('404');
 });
 
 // custom 500 page
 app.use(function(err, req, res, next) {
   console.error(err.stack);
-  res.type('text/plain');
   res.status(500);
-  res.send('500 - Server Error');
+  res.render('500');
 });
 
 app.listen(app.get('port'), function() {
