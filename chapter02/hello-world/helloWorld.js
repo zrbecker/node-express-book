c141502ffb53ed776deaa3798533d26ed482d3aa
Zachary Becker <Zachary Becker>
2017-03-10 20:43:35 -0800

    Added static file serving to hello world example

--- chapter02/hello-world/helloWorld.js
+++ chapter02/hello-world/helloWorld.js
@@ -1,4 +1,18 @@
 var http = require('http');
+var fs = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode) {
+  if (!responseCode) responseCode = 200;
+  fs.readFile(__dirname + path, function(err, data) {
+    if (err) { 
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('500 - Internal Error');
+    } else {
+      res.writeHead(responseCode, { 'Content-Type': contentType });
+      res.end(data);
+    }
+  });
+}
 
 http.createServer(function(req, res) {
   // normalize url by removing querystring, optional
@@ -6,18 +20,19 @@ http.createServer(function(req, res) {
   var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
   switch (path) {
     case '':
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Homepage');
+      serveStaticFile(res, '/public/home.html', 'text/html');
       break;
 
     case '/about':
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('About');
+      serveStaticFile(res, '/public/about.html', 'text/html');
+      break;
+
+    case '/img/logo.png':
+      serveStaticFile(res, '/public/img/logo.png', 'image/png');
       break;
 
     default:
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('Not Found');
+      serveStaticFile(res, '/public/notfound.html', 'text/html', 404);
       break;
   }
 }).listen(3000);
