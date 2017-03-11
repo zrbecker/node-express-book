ca39b2f38f4cba9691bff85a082fac70711f0b2e
Zachary Becker <Zachary Becker>
2017-03-11 14:07:37 -0800

    Added some pages and tests for cross page testing.

--- chapter05/meadowlark/site/meadowlark.js
+++ chapter05/meadowlark/site/meadowlark.js
@@ -30,6 +30,14 @@ app.get('/about', function(req, res) {
   });
 });
 
+app.get('/tours/hood-river', function(req, res) {
+  res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res) {
+  res.render('tours/request-group-rate');
+});
+
 // custom 404 page
 app.use(function(req, res, next) {
   res.status(404);
